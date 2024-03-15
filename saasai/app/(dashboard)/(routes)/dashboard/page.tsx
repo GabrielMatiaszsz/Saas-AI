9a7bb45d7958c49044bc2a1ccc162370ea6ba309
Gabriel Matias <gmsilva@uniara.edu.br>
2024-03-15 10:43:59 -0300

    iniciando dashboard

---
+++ saasai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const DashboardPage = () => {
+  return (
+<p>
+    dahsboard page (protected)
+</p>
+  );
+}
+export default DashboardPage;
---
+++ saasai/app/(landing)/page.tsx
@@ -0,0 +1,8 @@
+const LandingPage = () =>{
+    return(
+             <div>
+                Landing page (Unprotected)
+             </div>
+    )
+}
+export default LandingPage;
